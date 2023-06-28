ea33f2e4d00fc343e1e79704f2c25b8e8bda436c
Max Sporer <maxsporer@Maxs-MacBook-Pro.local>
2023-06-27 21:04:40 -0400

    add personal IoC container npm package, write preliminary tests to check functionality

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { ShoppingCart, Item } from './models/ShoppingCart';
+import { IoCContainer } from 'mspo-ioc-container/lib';
+
+const container = new IoCContainer();
 
 const cart = new ShoppingCart();
+container.bind('cart', cart);
+
 const apple = new Item('Apple', 1);
 const orange = new Item('Orange', 1.5);
 
 cart.addItem(apple, 1);
 cart.addItem(orange, 3);
 
-console.log(cart.getItems());
-console.log(cart.getTotalPrice());
+console.log(cart);
+console.log(container.resolve('cart'));
